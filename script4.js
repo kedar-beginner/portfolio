5f6a8b820cbffc2d30e78c951085f2912a691202
Kedar Kisan <157523862+kedar-beginner@users.noreply.github.com>
2024-06-23 20:51:52 +0530

    Update script4.js

--- script4.js
+++ script4.js
@@ -1,6 +1,28 @@
-function validateForm() {
-    let x = document.forms["myForm"]["name"].value;
-    if (x == "") {
-      alert("Name must be filled out");
-    }
+function validateForm(){
+  var returnval = true;
+
+  var name = document.forms['contactform']["name"].value;
+  if (name.length<3){
+    alert("Name is too short");
+      returnval = false;
   }
+
+  if (name.length == 0){
+      alert("Name can't be left blank")
+      returnval = false;
+  }
+
+  var email = document.forms['contactform']["email"].value;
+  if (email.length>30){
+      alert("email is too long");
+      returnval = false;
+  }
+
+  var phone = document.forms['contactform']["phone"].value;
+  if (phone.length != 10){
+      alert("*Phone number should be of 10 digits!");
+      returnval = false;
+  }
+
+  return returnval;
+}
